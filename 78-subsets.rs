a7c637f2ea9ab61f5d2c6f60429d8aac94bd64d8
sahil <404salad@gmail.com>
2023-11-13 17:29:21 +0530

    backtracking insight

--- 78-subsets.rs
+++ 78-subsets.rs
@@ -1,3 +1,13 @@
+
+Pick a starting point.
+while(Problem is not solved)
+    For each path from the starting point.
+        check if selected path is safe, if yes select it
+        and make recursive call to rest of the problem
+        before which undo the current move.
+    End For
+If none of the move works out, return false, NO SOLUTON.
+
 impl Solution {
     pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
         let mut combo = vec![];
